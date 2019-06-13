edb447e68bba443d24524404f41a5234218205f8
jacluff1 <jacluff1@asu.edu>
2019-06-12 20:29:26 -0400

    catch up

---
+++ pyspark.sh
@@ -0,0 +1,13 @@
+# install java
+sudo apt-get install openjdk-8-jre-headless
+
+# install pyspark
+pip install pyspark
+
+# # install docker
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+# sudo apt-get update
+# # apt-cache policy docker-ce
+# sudo apt-get install -y docker-ce
+# sudo docker run -p 8888:8888 jupyter/scipy-notebook:17aba6048f44
