3a227f2ae2decb1916b51a7cbaa771201cb7978b
jacluff1 <jacluff1@asu.edu>
2019-11-17 12:31:31 -0700

    update to give_permistions

--- give_permissions.sh
+++ give_permissions.sh
@@ -2,17 +2,20 @@
 
 # get all shell scripts from parent directory on down
 scripts=$(find -iname "*.sh")
+printf "\nfound scripts:\n${scripts[@]}\n\n"
 
 # for all the shell scripts found
-for script in "${scripts[@]}"; do
+for xx in ${scripts[@]}; do
     # skip current file
-    if [[ "${scripts[@]}" =~ "give_permissions.sh" ]]; then continue; fi
+    if [[ "give_permissions" == *"$xx"* ]]; then continue; fi
     # if script is in git repo
     if [ -f .git/config ]; then
         # add permissions via git
-        git update-index --chmod=+x $script;
+        git update-index --chmod=+x $xx
+        printf "added permissions to $xx via git\n";
     else
         # otherwise add permissions via chmod
-        chmod 755 $script;
+        chmod 755 $script
+        printf "added permissions to $xx via chmod\n";
     fi
 done
